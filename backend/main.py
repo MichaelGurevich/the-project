cc71455a808c0c0db1aeee2eec1d069655371c3f
Keren Stoller <keren123stoller@gmail.com>
2025-05-01 12:18:16 +0300

    moved jobs fetching and api's to dedicated microservice

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from 
+from microservices.jobs import router as jobs_router
+
 
 app = FastAPI()
+app.include_router(jobs_router)
 
 @app.get("/")
 def root():
---
+++ backend/microservices/jobs.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+jobs = ["Amazon", "Facebook", "Google", "Yahoo"]
+
+## path parameter
+@router.get("/jobs/{job_id}")
+def get_job(job_id: int):
+    if(job_id in jobs):
+        return jobs[job_id]
+    return None
+
+## query parameter
+@router.get("/jobs")
+def get_jobs(amountOfJobs: int = None):
+    if amountOfJobs:
+        return jobs[:amountOfJobs]      ## return the wanted amount
+    return jobs
+
+@router.get("/allJobs")
+async def get_all_jobs():
+    ## get from DB
+    return jobs
