d4e05e03c2dae9acd21050b5b01778ef5595e49f
Michael Gurevich <105605801+MichaelGurevich@users.noreply.github.com>
2025-05-01 21:34:34 +0300

    TWN-0332: fixes stuff

--- packages/frontend/mobile/app/(tabs)/_layout.tsx
+++ packages/frontend/mobile/app/(tabs)/_layout.tsx
@@ -1,64 +1,44 @@
-import React from "react";
-import { Tabs } from "expo-router";
+import React from 'react';
+import styled from 'styled-components/native';
+import { Tabs } from 'expo-router';
 
-import {
-  BottomTabBar,
-  BottomTabsIconsData,
-} from "../../components/BottomTabsBar";
+//import { BottomTabBar } from '../../components/BottomTabsBar';
 import { ThemeProvider } from "styled-components/native";
-import { lightTheme } from "@/theme/theme";
+import { lightTheme } from "../../theme/theme";
 
-const TAB_BAR_ICONS_PATH = '@/icons/BottomNavBarIcons/'
 
-const data: BottomTabsIconsData[] = [
-  {
-    pageName: "index",
-    iconSource: require(TAB_BAR_ICONS_PATH + "tab-icon-home-outline-black.png"),
-    focusedIconSource: require(TAB_BAR_ICONS_PATH + "tab-icon-home-filled-turquoise.png"),
-  },
-  {
-    pageName: "Search",
-    iconSource: require(TAB_BAR_ICONS_PATH + "tab-icon-search-outline-black.png"),
-    focusedIconSource: require( TAB_BAR_ICONS_PATH +  "tab-icon-search-filled-turquoise.png"),
-  },
-  {
-    pageName: "Discover",
-    iconSource: require(TAB_BAR_ICONS_PATH + "tab-icon-compass-outline-black.png"),
-    focusedIconSource: require(TAB_BAR_ICONS_PATH + "tab-icon-compass-filled-turquoise.png"),
-  },
-  {
-    pageName: "Saved",
-    iconSource: require(TAB_BAR_ICONS_PATH + "tab-icon-bookmark-outline-black.png"),
-    focusedIconSource: require(TAB_BAR_ICONS_PATH + "tab-icon-bookmark-filled-turquoise.png"),
-  },
+const TestView = styled.View`
+    height: 100px;
+    width: 50px;
+    background-color: red;
+`
 
-  {
-    pageName: "Profile",
-    iconSource: require(TAB_BAR_ICONS_PATH + "tab-icon-profile-outline-black.png"),
-    focusedIconSource: require(TAB_BAR_ICONS_PATH + "tab-icon-profile-filled-turquoise.png"),
-  },
-];
 
 export default function TabLayout() {
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-          tabBarShowLabel: false,
-        }}
-        tabBar={(props) => <BottomTabBar {...props} iconsData={data} />}
-      >
-        <Tabs.Screen name="index" />
+    return (
+        <ThemeProvider theme={lightTheme}>
+            <Tabs
+                //tabBar={props => <TabItem />}
+            >
 
-        <Tabs.Screen name="Search" />
+                <Tabs.Screen
+                    name="index"
+                    options={{
+                        title: 'Home',
+                        headerShown: false,
+                    }}
+                />
 
-        <Tabs.Screen name="Discover" />
 
-        <Tabs.Screen name="Saved" />
+                <Tabs.Screen
+                    name="Saved"
+                    options={{
+                        title: 'Saved',
+                        headerShown: false,
+                    }}
+                />
 
-        <Tabs.Screen name="Profile" />
-      </Tabs>
-    </ThemeProvider>
-  );
+            </Tabs>
+        </ThemeProvider>
+    );
 }
