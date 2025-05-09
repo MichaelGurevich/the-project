bb5092c82a1dcb765b67207d6e09aee3c2eb75d3
MichaelGurevich <michaelgur273@gmailc.com>
2025-05-09 23:13:58 +0300

    TWN-0017 feat(BottomNavigationBar): Bottom navigation rendering dynamically with dummy data

--- packages/frontend/mobile/app/(tabs)/_layout.tsx
+++ packages/frontend/mobile/app/(tabs)/_layout.tsx
@@ -1,36 +1,49 @@
-import React from 'react';
-import { Tabs } from 'expo-router';
-import { ThemeProvider, useTheme } from 'styled-components/native';
-import { lightTheme } from '../../theme/theme';
-import { IconContext } from 'phosphor-react-native';
-import { BottomNavigationBar } from '@/components/BottomNavigationTab';
+import React from "react";
+import { Tabs } from "expo-router";
+import { ThemeProvider, useTheme } from "styled-components/native";
+import { lightTheme } from "../../theme/theme";
+import { IconContext } from "phosphor-react-native";
+import { BottomNavigationBar } from "@/components/BottomNavigationTab";
+import { BottomNavItem } from "@/components/BottomNavigationTab/BottomNavItem";
+import { IconProps } from "@/components/Icon";
+
+export type tabIconsData = {
+  iconsData: IconProps[];
+};
+
+export type CustomTabScreenOptions = tabIconsData &
+  React.ComponentProps<typeof Tabs.Screen>["options"];
 
 function ThemedTabs() {
   const theme = useTheme();
 
   return (
-    <IconContext.Provider value={{
-      size:   theme.iconSizes.large,
-      color:  theme.palette.neutral[900],
-      weight: 'regular',
-    }}>
-      <Tabs
-        tabBar={() => (<BottomNavigationBar/>)}
-      >
+    <IconContext.Provider
+      value={{
+        size: theme.iconSizes.large,
+        color: theme.palette.neutral[900],
+        weight: "regular",
+      }}
+    >
+      <Tabs tabBar={(props) => <BottomNavigationBar {...props} />}>
         <Tabs.Screen
           name="index"
-          options={{
-            title: 'Home',
-            headerShown: false,
-          }}
+          options={
+            {
+              iconsData: [
+                {
+                  iconName: "House",
+                  weight: "regular",
+                },
+                {
+                  iconName: "House",
+                  weight: "fill",
+                },
+              ],
+            } as CustomTabScreenOptions
+          }
         />
-        <Tabs.Screen
-          name="Saved"
-          options={{
-            title: 'Saved',
-            headerShown: false,
-          }}
-        />
+        
       </Tabs>
     </IconContext.Provider>
   );
--- packages/frontend/mobile/components/BottomNavigationTab/BottomNavigationBar.tsx
+++ packages/frontend/mobile/components/BottomNavigationTab/BottomNavigationBar.tsx
@@ -2,7 +2,32 @@ import React from "react";
 import styled from "styled-components/native";
 import { BottomNavigationContainer } from "./BottomNavigationContainer";
 import { BottomNavItem } from "./BottomNavItem/BottomNavItem";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import {CustomTabScreenOptions} from '../../app/(tabs)/_layout';
 
-export const BottomNavigationBar = () => {
-  return <BottomNavigationContainer></BottomNavigationContainer>;
+type CustomTabOptions = CustomTabScreenOptions & {
+  iconsData?: { iconName: string; weight?: string }[];
+};
+
+export const BottomNavigationBar = ({
+  state,
+  descriptors,
+  navigation,
+}: BottomTabBarProps) => {
+  return (
+    <BottomNavigationContainer>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const customOptions = options as CustomTabScreenOptions;
+        console.log(options);
+
+        return (
+          <BottomNavItem
+            iconName={customOptions?.iconsData?.[0].iconName ?? "House"}
+            onPress={() => console.log("hello")}
+          />
+        );
+      })}
+    </BottomNavigationContainer>
+  );
 };
